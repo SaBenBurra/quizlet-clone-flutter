83a9b37c07726bbb33144f1813d4c3c03b5a7289
Burra <gezerbugra@gmail.com>
2023-03-31 20:27:07 +0300

    CustomTextFormField updated

--- lib/widgets/custom_text_form_field.dart
+++ lib/widgets/custom_text_form_field.dart
@@ -1,13 +1,17 @@
 import 'package:flutter/material.dart';
 
 TextFormField buildCustomTextFormField(
-    {TextEditingController? controller, String? Function(String?)? validator}) {
+    {TextEditingController? controller,
+    String? Function(String?)? validator,
+    double? fontSize}) {
   return TextFormField(
       cursorColor: Colors.white,
       validator: validator,
       decoration: const InputDecoration(
+          enabledBorder:
+              UnderlineInputBorder(borderSide: BorderSide(color: Colors.white)),
           focusedBorder: UnderlineInputBorder(
               borderSide: BorderSide(color: Colors.yellow))),
-      style: TextStyle(color: Colors.white),
+      style: TextStyle(color: Colors.white, fontSize: fontSize),
       controller: controller);
 }
